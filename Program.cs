65dbcc37daef73fecffd2bb1ba9d4887c93365de
Jakob Andersson <jakob.aah@gmail.com>
2019-11-04 08:43:14 +0100

    redo

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PizzaPalatsetG1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string arg = args[0];
+            }
+            catch(IndexOutOfRangeException){
+                Console.Write("Rätt .exe filer ej tillagda. Saknar startargument");
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# PizzaPalatsetG1
-ProjektArbete Obj.grundKurs 2019 grupp 1
