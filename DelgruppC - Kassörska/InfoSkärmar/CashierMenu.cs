6b523564a73e61d9b512211420023d2f70586f69
Harliin <56713048+Harliin@users.noreply.github.com>
2019-11-06 11:18:48 +0100

    person klass klar

---
+++ DelgruppC - Kassörska/InfoSkärmar/CashierMenu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfoSkärmar
+{
+    public class CashierMenu
+    {
+        public void Login()
+        {
+
+        }
+    }
+}
--- DelgruppC - Kassörska/InfoSkärmar/Program.cs
+++ DelgruppC - Kassörska/InfoSkärmar/Program.cs
@@ -6,7 +6,12 @@ namespace InfoSkärmar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer customer = new Customer();
+            Employee employee = new Employee();
+            Console.WriteLine($"Customer: {customer._name} ID: {customer._ID} Kredit: {customer._kredit}");
+            Customer customer2 = new Customer();
+            Console.WriteLine($"Customer: {customer2._name} ID: {customer2._ID} Kredit: {customer2._kredit}");
+            Console.WriteLine($"Employee: {employee._name} ID: {employee._ID} Password: {employee._password}");
         }
     }
 }
