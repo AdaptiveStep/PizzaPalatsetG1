83e70fa5eb78104e97862e34e2ee5a1a19f5b6cd
Jakob Andersson III <jakob.aah@gmail.com>
2019-11-12 15:01:57 +0100

    BEEP BOOP

--- DelgruppB - Bagare/Beeper.cs
+++ DelgruppB - Bagare/Beeper.cs
@@ -8,5 +8,36 @@ namespace DelgruppB___Bagare
 {
     class Beeper
     {
+        protected System.Media.SoundPlayer player;
+        List<Sound> Sounds = new List<Sound>();
+
+        public Beeper()
+        {
+            //string current_dir = System.Environment.CurrentDirectory;
+            //string audio_dir = System.IO.Path.Combine(current_dir, @"audio/beep1.wav");
+        }
+   
+
+ //        protected player;
+ // List<Sound> Sounds=new List<Sound>()
+
+ //private LoadFromFolder()
+ // "Folder" foreach=>
+ //    var _ new = Sound()
+ //    _.data= File.ReadAllBytes(@"in.wav"
+ //  _.name = filename
+ //    Sounds.add(_)
+
+ //public Beeper()
+ // Player player = new Player()
+ //LoadFromFolder("/audio"
+ //public Beeper(string foldername)
+
+ //public Play(string title)
+ // try{
+ //  bar sound = Sounds.get(title)
+ //  sound.Play()
+ //}
+ //catch()
     }
 }
