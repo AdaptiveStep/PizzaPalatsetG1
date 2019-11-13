771817710c97457733608966bb8b2923c2773a2c
Hitman08 <56691277+Hitman08@users.noreply.github.com>
2019-11-13 23:34:51 +0100

    Bagare terminal complete EJ

---
+++ DelgruppB - Bagare/PizzamakerTerminal v.1.0 Elchin/PizzamakerTerminal/Cock.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PizzamakerTerminal
+{
+    class Cock
+    {
+        // Inloggningsmeny för bagare
+        public static void LogIn()
+        {
+        Console.WriteLine("\nVälkommen till Pizzeria Palatsen!");
+
+            // do-while loop dyker så länge upp tills användaren inmater korrekta uppgifter 
+            do
+            {
+                // Inlogningsinformation visas på skärmen som hjälpmedel
+                    Console.WriteLine("\t\t\t\t\t\t\t\t\tInlogningsinformation: ");
+                    Console.WriteLine("\t\t\t\t\t\t\t\t\tanvändernamn: user1 >> lösenord: 1111");
+                    Console.WriteLine("\t\t\t\t\t\t\t\t\tanvändernamn: user2 >> lösenord: 2222");
+                    Console.WriteLine("\t\t\t\t\t\t\t\t\tanvändernamn: user3 >> lösenord: 3333");
+                    Console.WriteLine("\t\t\t\t\t\t\t\t\tanvändernamn: user4 >> lösenord: 4444");
+
+                // Tar input uppgifter från bagare
+                    Console.Write("\nSkriv ditt användarnamn: ");
+                    string username = Console.ReadLine();
+                    Console.Write("Ange lösenord: ");
+                    string password = Console.ReadLine();
+
+                // Programmet kollar upp vilken bagare som loggar in
+                    if (username == "user1" && password == "1111")
+                    {
+                    Console.Clear();
+                        Console.WriteLine("Välkommen User1.\n\nTryck på någon knapp att komma till beställningsmenyn");
+                    Console.ReadKey();
+                        User.OrderTerminal();
+                    }
+                    else if (username == "user2" && password == "2222")
+                    {
+                    Console.Clear();
+                    Console.WriteLine("Välkommen User2.\n\nTryck på någon knapp att komma till beställningsmenyn");
+                    Console.ReadKey();
+                    User.OrderTerminal();
+                }
+                    else if (username == "user3" && password == "3333")
+                    {
+                    Console.Clear();
+                    Console.WriteLine("Välkommen User3.\n\nTryck på någon knapp att komma till beställningsmenyn");
+                    Console.ReadKey();
+                    User.OrderTerminal();
+                }
+                    else if (username == "user4" && password == "4444")
+                    {
+                    Console.Clear();
+                    Console.WriteLine("Välkommen User4.\n\nTryck på någon knapp att komma till beställningsmenyn");
+                    Console.ReadKey();
+                    User.OrderTerminal();
+                }
+                    else
+                    {
+                        Console.WriteLine("\nFelaktig användarnamn eller lösenord. Försök igen");
+                    Console.ReadKey();
+                    Console.Clear();
+                    LogIn();
+                    }
+            } while (false);
+
+        }
+    }
+}
---
+++ DelgruppB - Bagare/PizzamakerTerminal v.1.0 Elchin/PizzamakerTerminal/OrderSelection.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PizzamakerTerminal
+{
+    class OrderSelection
+    {
+        public static List<Order> activeOrders = new List<Order>();
+
+        // Metod som hjälper välja valet
+        public static void OrderChoice()
+        {
+            short orderNumber = 0;
+            while (true)
+            {
+                ConsoleKeyInfo action = Console.ReadKey();
+
+                // Vid Enter knappen adderas ny beställning på beställningslista. 
+                //Vid valet mellan 1 och 9 öppnas slumpa beställningarsinformation.
+                if (action.Key == ConsoleKey.Enter)
+                {
+                    orderNumber++;
+                    OrderReceive(orderNumber);
+                }
+                if (action.Key == ConsoleKey.NumPad1)
+                {
+                    activeOrders.Add(new Order("Beställning 1"));
+                    OrderInfo.ActivateOrder(1);
+                }
+                if (action.Key == ConsoleKey.NumPad2)
+                {
+                    activeOrders.Add(new Order("Beställning 2"));
+                    OrderInfo.ActivateOrder(2);
+                }
+                if (action.Key == ConsoleKey.NumPad3)
+                {
+                    activeOrders.Add(new Order("Beställning 3"));
+                    OrderInfo.ActivateOrder(3);
+                }
+                if (action.Key == ConsoleKey.NumPad4)
+                {
+                    activeOrders.Add(new Order("Beställning 4"));
+                    OrderInfo.ActivateOrder(4);
+                }
+                if (action.Key == ConsoleKey.NumPad5)
+                {
+                    activeOrders.Add(new Order("Beställning 5"));
+                    OrderInfo.ActivateOrder(5);
+                }
+                if (action.Key == ConsoleKey.NumPad6)
+                {
+                    activeOrders.Add(new Order("Beställning 6"));
+                    OrderInfo.ActivateOrder(6);
+                }
+                if (action.Key == ConsoleKey.NumPad7)
+                {
+                    activeOrders.Add(new Order("Beställning 7"));
+                    OrderInfo.ActivateOrder(7);
+                }
+                if (action.Key == ConsoleKey.NumPad8)
+                {
+                    activeOrders.Add(new Order("Beställning 8"));
+                    OrderInfo.ActivateOrder(8);
+                }
+                if (action.Key == ConsoleKey.NumPad9)
+                {
+                    activeOrders.Add(new Order("Beställning 9"));
+                    OrderInfo.ActivateOrder(9);
+                }
+                // Om ingen passande val blev valt OrderTerminal() metod körs igen
+                if (action.Key != ConsoleKey.Enter && action.Key != ConsoleKey.NumPad1 && action.Key != ConsoleKey.NumPad2
+                    && action.Key != ConsoleKey.NumPad3 && action.Key != ConsoleKey.NumPad4 && action.Key != ConsoleKey.NumPad5
+                    && action.Key != ConsoleKey.NumPad6 && action.Key != ConsoleKey.NumPad7 && action.Key != ConsoleKey.NumPad8
+                    && action.Key != ConsoleKey.NumPad9)
+                {
+                    Console.WriteLine("\nFel val. Försök igen");
+                    Console.ReadKey();
+                    User.OrderTerminal();
+                }
+            }
+        }
+
+        // Vid Enter knappen har valt ny beställning visas på skärmen
+        public static void OrderReceive(short orderNumber)
+        {
+            Random random = new Random();
+
+            Console.WriteLine($"Beställning #{orderNumber}");
+            Console.Beep();
+            orderNumber++;
+        }
+    }
+}
