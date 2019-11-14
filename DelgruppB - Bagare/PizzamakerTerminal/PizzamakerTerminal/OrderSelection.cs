4cc9f86adb863c328a73a8bd664afe88846310c3
Jakob Andersson III <jakob.aah@gmail.com>
2019-11-14 13:20:16 +0100

    Struktur + Cleanup del i - jkb

--- DelgruppB - Bagare/PizzamakerTerminal/PizzamakerTerminal/OrderSelection.cs
+++ DelgruppB - Bagare/PizzamakerTerminal/PizzamakerTerminal/OrderSelection.cs
@@ -7,7 +7,6 @@ namespace PizzamakerTerminal
     {
         public static List<ActiveOrder> activeOrders = new List<ActiveOrder>();
 
-
         // Metod som hjälper välja valet
         public static void OrderChoice()
         {
@@ -15,77 +14,34 @@ namespace PizzamakerTerminal
             while (true)
             {
                 ConsoleKeyInfo action = Console.ReadKey();
+                string input = action.KeyChar.ToString();
 
-                // Vid Enter knappen adderas ny beställning på beställningslista. 
-                // Vid valet mellan 1 och 9 öppnas slumpa beställningarsinformation.
-                // Vid subtract '-' valet raderas den första klargjorda beställningen från listan.
-                if (action.Key == ConsoleKey.Enter)
+                // Enter        => Ny beställning simuleras 
+                // [1 - 9]      => Öppnas slumpa beställningarsinformation.
+                // subtract '-' => Valet raderas den första klargjorda beställningen från listan.
+
+                if (int.TryParse(input,out int res) && res >= 1 && res <= 9)
+                {
+                    OrderInfo.ActivateOrder(res);
+                }
+                else if (action.Key == ConsoleKey.Enter)
                 {
                     int orderNumber = initialOrder + activeOrders.Count;
                     OrderReceive(orderNumber);
                 }
-                if (action.Key == ConsoleKey.NumPad1)
-                {
-                    OrderInfo.ActivateOrder(1);
-                }
-                if (action.Key == ConsoleKey.NumPad2)
-                {
-
-                    OrderInfo.ActivateOrder(2);
-                }
-                if (action.Key == ConsoleKey.NumPad3)
-                {
-
-                    OrderInfo.ActivateOrder(3);
-                }
-                if (action.Key == ConsoleKey.NumPad4)
-                {
-
-                    OrderInfo.ActivateOrder(4);
-                }
-                if (action.Key == ConsoleKey.NumPad5)
-                {
-
-                    OrderInfo.ActivateOrder(5);
-                }
-                if (action.Key == ConsoleKey.NumPad6)
-                {
-
-                    OrderInfo.ActivateOrder(6);
-                }
-                if (action.Key == ConsoleKey.NumPad7)
-                {
-
-                    OrderInfo.ActivateOrder(7);
-                }
-                if (action.Key == ConsoleKey.NumPad8)
-                {
-
-                    OrderInfo.ActivateOrder(8);
-                }
-                if (action.Key == ConsoleKey.NumPad9)
-                {
-
-                    OrderInfo.ActivateOrder(9);
-                }
-                if (action.Key == ConsoleKey.Subtract)
+                else if (action.Key == ConsoleKey.Subtract)
                 {
                     DismissOrder();
                 }
-                if (action.Key == ConsoleKey.NumPad0)
+                else if (action.Key == ConsoleKey.NumPad0)
                 {
                     Console.Clear();
                     Program.Main();
                 }
-                // Om ingen passande val har blivit valt OrderTerminal() metod körs igen
-                if (action.Key != ConsoleKey.Enter && action.Key != ConsoleKey.NumPad1 && action.Key != ConsoleKey.NumPad2
-                    && action.Key != ConsoleKey.NumPad3 && action.Key != ConsoleKey.NumPad4 && action.Key != ConsoleKey.NumPad5
-                    && action.Key != ConsoleKey.NumPad6 && action.Key != ConsoleKey.NumPad7 && action.Key != ConsoleKey.NumPad8
-                    && action.Key != ConsoleKey.NumPad9 && action.Key != ConsoleKey.Subtract && action.Key != ConsoleKey.NumPad0)
+                else
                 {
-                    Console.WriteLine("\nFel val. Försök igen");
+                    Console.WriteLine("\nFel val. Försök igen"); // Om ingen passande val har blivit valt OrderTerminal() metod körs igen
                     Console.ReadKey();
-                    User.OrderTerminal(Cock.user);
                 }
             }
         }
