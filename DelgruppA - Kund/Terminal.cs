fe16b7b990abc9c3e0ed65e9c248a81bff22ba23
AdaptiveStep <hariz@kth.se>
2019-11-18 20:49:43 +0100

    fixat tabbar och återställt låsning med enter

--- DelgruppA - Kund/Terminal.cs
+++ DelgruppA - Kund/Terminal.cs
@@ -8,138 +8,145 @@ using System.Threading;
 
 namespace KundTermiPizzaPalatset
 {
-    public class Terminal
-    {
-        internal List<List<Product>> itemRegister = new List<List<Product>>();
+	public class Terminal
+	{
+		internal List<List<Product>> itemRegister = new List<List<Product>>();
 
-        public Terminal()
-        {
-            //Innehåller standardgreher
-            List<Product> pizzor = new List<Product>();
-            pizzor.Add(new Pizza ("Capricosa"));
-            pizzor.Add(new Pizza ( "Vesuvio"));
-            pizzor.Add(new Pizza ( "Vegetarisk", 50));
-            pizzor.Add(new Pizza ( "Mexicana"));
-            pizzor.Add(new Pizza ( "Hawaii"));
-            pizzor.Add(new Pizza ( "Salami"));
-            itemRegister.Add(pizzor);
+		public Terminal()
+		{
+			//Innehåller standardgreher
+			List<Product> pizzor = new List<Product>();
+			pizzor.Add(new Pizza ("Capricosa"));
+			pizzor.Add(new Pizza ( "Vesuvio"));
+			pizzor.Add(new Pizza ( "Vegetarisk", 50));
+			pizzor.Add(new Pizza ( "Mexicana"));
+			pizzor.Add(new Pizza ( "Hawaii"));
+			pizzor.Add(new Pizza ( "Salami"));
+			itemRegister.Add(pizzor);
 
-            //ANALogt
-            List<Product> pasta = new List<Product>();
-            pasta.Add(new Pasta ("Carbonara", 75));
-            pasta.Add(new Pasta ("Frutti di mare", 75));
-            pasta.Add(new Pasta ("Pesto", 75));
-            pasta.Add(new Pasta ("Mustafas krämiga Oxfilé"));
-            itemRegister.Add(pasta);
+			//ANALogt
+			List<Product> pasta = new List<Product>();
+			pasta.Add(new Pasta ("Carbonara", 75));
+			pasta.Add(new Pasta ("Frutti di mare", 75));
+			pasta.Add(new Pasta ("Pesto", 75));
+			pasta.Add(new Pasta ("Mustafas krämiga Oxfilé"));
+			itemRegister.Add(pasta);
 
-            //etc
-            List<Product> sallad = new List<Product>(); //todo
-            sallad.Add(new Sallad ("Räksallad"));
-            sallad.Add(new Sallad ("Fetaost-sallad"));
-            sallad.Add(new Sallad ("Ceasarsallad"));
-            sallad.Add(new Sallad ("Kycklingsallad"));
-            itemRegister.Add(sallad);
+			//etc
+			List<Product> sallad = new List<Product>(); //todo
+			sallad.Add(new Sallad ("Räksallad"));
+			sallad.Add(new Sallad ("Fetaost-sallad"));
+			sallad.Add(new Sallad ("Ceasarsallad"));
+			sallad.Add(new Sallad ("Kycklingsallad"));
+			itemRegister.Add(sallad);
 
-            List<Product> tillbehör = new List<Product>(); //todo
-            tillbehör.Add(new Tillbehör("Läsk, 33cl"));
-            tillbehör.Add(new Tillbehör("Läsk, 2L"));
-            tillbehör.Add(new Tillbehör("Dippsås, Bearnaise"));
-            tillbehör.Add(new Tillbehör("Dippsås, Vitlök"));
-            tillbehör.Add(new Tillbehör("Stor Stark"));
-            tillbehör.Add(new Tillbehör("Vin 40cl: Vitt"));
-            tillbehör.Add(new Tillbehör("Vin 40cl: Rött"));  //todo bank id, eventuell ny lista
-            itemRegister.Add(tillbehör);
-            //tillbehör.Add(new Tillbehör ("Ost")); //todo
-            //tillbehör.Add(new Tillbehör ("Skinka"));
-            //tillbehör.Add(new Tillbehör ("Oliver"));
-            //tillbehör.Add(new Tillbehör ("Jalapeño"));
-            //tillbehör.Add(new Tillbehör ("Ananas"));
-            //tillbehör.Add(new Tillbehör ("Lök"));
-            //tillbehör.Add(new Tillbehör ("Räkor"));
-            //tillbehör.Add(new Tillbehör ("Champinjoner"));
+			List<Product> tillbehör = new List<Product>(); //todo
+			tillbehör.Add(new Tillbehör("Läsk, 33cl"));
+			tillbehör.Add(new Tillbehör("Läsk, 2L"));
+			tillbehör.Add(new Tillbehör("Dippsås, Bearnaise"));
+			tillbehör.Add(new Tillbehör("Dippsås, Vitlök"));
+			tillbehör.Add(new Tillbehör("Stor Stark"));
+			tillbehör.Add(new Tillbehör("Vin 40cl: Vitt"));
+			tillbehör.Add(new Tillbehör("Vin 40cl: Rött"));  //todo bank id, eventuell ny lista
+			itemRegister.Add(tillbehör);
+			//tillbehör.Add(new Tillbehör ("Ost")); //todo
+			//tillbehör.Add(new Tillbehör ("Skinka"));
+			//tillbehör.Add(new Tillbehör ("Oliver"));
+			//tillbehör.Add(new Tillbehör ("Jalapeño"));
+			//tillbehör.Add(new Tillbehör ("Ananas"));
+			//tillbehör.Add(new Tillbehör ("Lök"));
+			//tillbehör.Add(new Tillbehör ("Räkor"));
+			//tillbehör.Add(new Tillbehör ("Champinjoner"));
 
 
-            List<Order> ordrar = new List<Order>();
-            List<Customer> customers = new List<Customer>();
-          
-        }
+			List<Order> ordrar = new List<Order>();
+			List<Customer> customers = new List<Customer>();
+		  
+		}
  
 
 
-        public static int  IntIn()
-        {
-            Console.WriteLine();
+		public static int  IntIn()
+		{
+			Console.WriteLine();
 
-            // bytte ut TryParse / ConsoleReadLine till en Consolekey istället.
-            ConsoleKeyInfo info = Console.ReadKey(true);
-            //string infot = Convert.ToChar(info);   
-            //Int32.TryParse(Console.ReadLine(), out int input);
+			// bytte ut TryParse / ConsoleReadLine till en Consolekey istället.
+			ConsoleKeyInfo info = Console.ReadKey(true);
+			//string infot = Convert.ToChar(info);   
+			//Int32.TryParse(Console.ReadLine(), out int input);
 
-            bool done = false;
-            //int input = Convert.ToInt32(Console.ReadLine());
-            while (!done)
-            {
-                if(info.KeyChar>47 && info.KeyChar<58)
-                if (info.KeyChar.ToString() == "1")
-                {
+			bool done = false;
+			//int input = Convert.ToInt32(Console.ReadLine());
+			while (!done)
+			{
+				//if(info.KeyChar>47 && info.KeyChar<58){
+				//	Console.WriteLine("inge fuffens nu");
+				//	continue;
+				//}
 
-                    return 1;
-                }
-                else if (info.Key == ConsoleKey.D2)
-                {
-                    return 2;
-                }
-                else if (info.Key == ConsoleKey.D3)
-                {
-                    return 3;
-                }
-                else if (info.Key == ConsoleKey.D4)
-                {
-                    return 4;
-                }
-                else if (info.Key == ConsoleKey.D5)
-                {
-                    return 5;
-                }
-                else if (info.Key == ConsoleKey.D6)
-                {
-                    return 6;
-                }
-                else if (info.Key == ConsoleKey.D7)
-                {
-                    return 7;
-                }
-                else if (info.Key == ConsoleKey.D8)
-                {
-                    return 8;
-                }
-                else if (info.Key == ConsoleKey.D9)
-                {
-                    return 9;
-                }
-                else
-                { Console.WriteLine("\t\t\t\t\tFelaktig inmatning! försök igen"); Thread.Sleep(200); break; }
-                
-                //try
-                //{
-                    
-                //    done = true;
-                //    return input;
+				if (info.KeyChar.ToString() == "1")
+				{
 
-                //}
-                //catch(FormatException e)
-                //{
-                //    Console.WriteLine("Nått gick galet, försök igen!", e.Message);
-                    
-                //}
+					return 1;
+				}
+				else if (info.Key == ConsoleKey.D2)
+				{
+					return 2;
+				}
+				else if (info.Key == ConsoleKey.D3)
+				{
+					return 3;
+				}
+				else if (info.Key == ConsoleKey.D4)
+				{
+					return 4;
+				}
+				else if (info.Key == ConsoleKey.D5)
+				{
+					return 5;
+				}
+				else if (info.Key == ConsoleKey.D6)
+				{
+					return 6;
+				}
+				else if (info.Key == ConsoleKey.D7)
+				{
+					return 7;
+				}
+				else if (info.Key == ConsoleKey.D8)
+				{
+					return 8;
+				}
+				else if (info.Key == ConsoleKey.D9)
+				{
+					return 9;
+				}
+				else
+				{ Console.WriteLine("\t\t\t\t\tFelaktig inmatning! försök igen"); 
+				Thread.Sleep(200); 
+				//break; 
+				}
+				
+				//try
+				//{
+					
+				//    done = true;
+				//    return input;
 
-               
-            }
-            return 0;  //return input;
-            //return input;
-        } //tryparse readkey
-        
-        
-    }
+				//}
+				//catch(FormatException e)
+				//{
+				//    Console.WriteLine("Nått gick galet, försök igen!", e.Message);
+					
+				//}
+
+			   
+			}
+			return 0;  //return input;
+			//return input;
+		} //tryparse readkey
+		
+		
+	}
 
 }
